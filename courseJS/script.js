ef35ed42fcb0c1fd610815944dac77400a6055e9
axel <huertaaxel1003@gmail.com>
2022-06-16 22:41:59 -0300

    funciones

---
+++ courseJS/script.js
@@ -0,0 +1,21 @@
+let p1 = 1
+let p2 = 10
+
+function suma(p1,p2){
+    return p1 +p2
+}
+console.log(suma(p1,p2))
+
+function resta(p1,p2){
+    resultado= p1 - p2
+    console.log(resultado)
+}
+
+resta(p1,p2)
+
+console.warn("objetos")
+
+const car = {tipo:"marca", color:"rojo", peso:200, ruedas:4}
+
+console.log(car.tipo)
+
